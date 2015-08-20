8d03a9641b334764f463bd84a69dc37c0946ab88
Matt <matt@pixotech.com>
2015-08-20 16:52:20 -0500

    Twig environment constructor

--- src/Twig/Environment.php
+++ src/Twig/Environment.php
@@ -2,6 +2,14 @@
 
 namespace Labcoat\Twig;
 
+use Labcoat\PatternLab;
+
 class Environment extends \Twig_Environment {
 
+  protected $patternlab;
+
+  public function __construct(PatternLab $patternlab, array $options = []) {
+    $loader = new Loader($patternlab);
+    parent::__construct($loader, $options);
+  }
 }
