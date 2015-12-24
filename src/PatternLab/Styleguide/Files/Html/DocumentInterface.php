3fd080acb8e497a9401116eb0f7b88d216643d8b
Matt Sharkey <matt@pixotech.com>
2015-12-24 14:25:52 -0600

    Get rid of document subclasses

--- src/PatternLab/Styleguide/Files/Html/DocumentInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Labcoat\PatternLab\Styleguide\Files\Html;
-
-use Labcoat\Html\DocumentInterface as HtmlDocumentInterface;
-
-interface DocumentInterface extends HtmlDocumentInterface {
-
-}
