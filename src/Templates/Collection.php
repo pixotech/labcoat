978849942e26ad24d35493b6eaf842d301682f56
Matt Sharkey <matt@pixotech.com>
2016-02-24 17:20:44 -0600

    Add Collection::getLoader() method

--- src/Templates/Collection.php
+++ src/Templates/Collection.php
@@ -4,17 +4,34 @@ namespace Labcoat\Templates;
 
 class Collection implements CollectionInterface {
 
+  /**
+   * @var TemplateInterface[]
+   */
   protected $templates = [];
 
+  /**
+   * @param TemplateInterface $template
+   */
   public function add(TemplateInterface $template) {
     $this->templates[] = $template;
   }
 
+  /**
+   * @param $name
+   * @return TemplateInterface
+   */
   public function find($name) {
     foreach ($this->getTemplates() as $template) {
       if ($template->is($name)) return $template;
     }
     throw new \OutOfBoundsException("Unknown template: $name");
+  }
+
+  /**
+   * @return Loader
+   */
+  public function getLoader() {
+    return new Loader($this);
   }
 
   /**
--- src/Templates/CollectionInterface.php
+++ src/Templates/CollectionInterface.php
@@ -3,6 +3,21 @@
 namespace Labcoat\Templates;
 
 interface CollectionInterface {
+
+  /**
+   * @param TemplateInterface $template
+   */
   public function add(TemplateInterface $template);
+
+  /**
+   * @param $name
+   * @return TemplateInterface
+   * @throws \OutOfBoundsException
+   */
   public function find($name);
+
+  /**
+   * @return LoaderInterface
+   */
+  public function getLoader();
 }
