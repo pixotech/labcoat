42b2a7143c8f8f7f9766bca26b9a4d6f91321230
Matt Sharkey <matt@pixotech.com>
2016-02-24 19:04:25 -0600

    Stub out included/including pattern methods

--- src/PatternLab/Styleguide/Patterns/Pattern.php
+++ src/PatternLab/Styleguide/Patterns/Pattern.php
@@ -16,14 +16,6 @@ class Pattern extends AbstractPattern {
 
   protected $valid;
 
-  public static function isIncludeToken(\Twig_Token $token) {
-    return self::isNameToken($token) && in_array($token->getValue(), ['include', 'extend']);
-  }
-
-  public static function isNameToken(\Twig_Token $token) {
-    return $token->getType() == \Twig_Token::NAME_TYPE;
-  }
-
   public function __construct(SourceInterface $source, PatternRendererInterface $renderer) {
     parent::__construct($renderer);
     $this->source = $source;
@@ -42,11 +34,7 @@ class Pattern extends AbstractPattern {
   }
 
   public function getIncludedPatterns() {
-    $included = [];
-    foreach ($this->includedPatterns as $pattern) {
-      #$included[] = $this->patternlab->get($pattern);
-    }
-    return $included;
+    return [];
   }
 
   public function getIncludingPatterns() {
@@ -114,34 +102,5 @@ class Pattern extends AbstractPattern {
   public function matches($name) {
     if (PatternLab::isPartialName($name)) return $name == $this->getPartial();
     else return (string)PatternLab::normalizePath($name) == (string)PatternLab::normalizePath($this->getPath());
-  }
-
-  /**
-   * @return \Twig_TokenStream
-   * @throws \Twig_Error_Syntax
-   */
-  protected function getTemplateTokens() {
-    $template = file_get_contents($this->getFile());
-    $lexer = new \Twig_Lexer(new \Twig_Environment());
-    return $lexer->tokenize($template);
-  }
-
-  protected function parseTemplate() {
-    $this->valid = true;
-    $this->includedPatterns = [];
-    try {
-      $tokens = $this->getTemplateTokens();
-      while (!$tokens->isEOF()) {
-        $token = $tokens->next();
-        if (self::isIncludeToken($token)) {
-          $next = $tokens->next()->getValue();
-          if ($next == '(') $next = $tokens->next()->getValue();
-          $this->includedPatterns[] = $next;
-        }
-      }
-    }
-    catch (\Twig_Error_Syntax $e) {
-      $this->valid = false;
-    }
   }
 }
