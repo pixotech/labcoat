270d3992ace71b5dffc052bf1369cfb5e78f980b
Matt Sharkey <matt@pixotech.com>
2016-03-01 14:27:08 -0600

    Pattern tests

---
+++ test/PatternLab/Patterns/PatternTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Labcoat\PatternLab\Patterns;
+
+class PatternTest extends \PHPUnit_Framework_TestCase {
+
+  public function testDescription() {
+    $description = 'This is the description of the pattern';
+    $pattern = $this->makePattern();
+    $pattern->setDescription($description);
+    $this->assertEquals($description, $pattern->getDescription());
+  }
+
+  public function testExample() {
+    $example = '<p>This is the <dfn>pattern example</dfn>.</p>';
+    $pattern = $this->makePattern();
+    $pattern->setExample($example);
+    $this->assertEquals($example, $pattern->getExample());
+  }
+
+  public function testLabel() {
+    $label = 'Label';
+    $pattern = $this->makePattern();
+    $pattern->setLabel($label);
+    $this->assertEquals($label, $pattern->getLabel());
+  }
+
+  public function testDefaultLabel() {
+    $name = 'the-pattern-name';
+    $pattern = $this->makePattern($name);
+    $this->assertEquals("The Pattern Name", $pattern->getLabel());
+  }
+
+  public function testName() {
+    $name = 'pattern-name';
+    $pattern = $this->makePattern($name);
+    $this->assertEquals($name, $pattern->getName());
+  }
+
+  public function testState() {
+    $state = 'state';
+    $pattern = $this->makePattern();
+    $pattern->setState($state);
+    $this->assertEquals($state, $pattern->getState());
+  }
+
+  public function testSubtype() {
+    $subtype = 'subtype';
+    $pattern = $this->makePattern('name', 'type', $subtype);
+    $this->assertEquals($subtype, $pattern->getSubtype());
+  }
+
+  public function testHasSubtype() {
+    $subtype = 'subtype';
+    $pattern = $this->makePattern('name', 'type', $subtype);
+    $this->assertTrue($pattern->hasSubtype());
+  }
+
+  public function testNoSubtype() {
+    $pattern = $this->makePattern('name', 'type');
+    $this->assertFalse($pattern->hasSubtype());
+  }
+
+  public function testType() {
+    $type = 'type';
+    $pattern = $this->makePattern('name', $type);
+    $this->assertEquals($type, $pattern->getType());
+  }
+
+  protected function makePattern($name = 'name', $type = 'type', $subtype = null) {
+    return new Pattern($name, $type, $subtype);
+  }
+}
